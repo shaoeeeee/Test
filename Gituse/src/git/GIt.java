6b8278fb7dd60bc07329788b23af77e087afe6f1
dopamine <harmonious52@163.com>
2019-07-01 16:59:02 +0800

    测试

--- Gituse/src/git/GIt.java
+++ Gituse/src/git/GIt.java
@@ -4,4 +4,8 @@ public class GIt {
     public static void main(String[] args) {
         System.out.println("gitgitgitgitgitigit");
     }
+
+    public void test{
+        System.out.println("aaaaaa");
+    }
 }
